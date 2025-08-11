16cf308a1ebe9d7a92383c8273b6b4495994cd18
김관응 <whatsup95@naver.com>
2025-08-11 23:52:01 +0900

    chore: 샘플 모달 커밋

--- apps/web-main/src/app/sample/page.tsx
+++ apps/web-main/src/app/sample/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 const SamplePage = () => {
   return (
     <div>
@@ -21,18 +23,20 @@ const SamplePage = () => {
           </li>
         </ul>
       </details>
-      <button className="btn">open modal</button>
-      <dialog id="my_modal_1" className="modal">
+      <button
+        className="btn"
+        onClick={() => (document.getElementById('my_modal_2') as HTMLDialogElement)?.showModal()}
+      >
+        open modal
+      </button>
+      <dialog id="my_modal_2" className="modal">
         <div className="modal-box">
           <h3 className="font-bold text-lg">Hello!</h3>
-          <p className="py-4">Press ESC key or click the button below to close</p>
-          <div className="modal-action">
-            <form method="dialog">
-              {/* if there is a button in form, it will close the modal */}
-              <button className="btn">Close</button>
-            </form>
-          </div>
+          <p className="py-4">Press ESC key or click outside to close</p>
         </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
       </dialog>
     </div>
   );
