c9bb3c428902dca67acba942be2aa2be22e2afb5
김관응 <whatsup95@naver.com>
2025-08-11 23:42:19 +0900

    chore: 샘플 커밋

--- apps/web-main/src/app/sample/page.tsx
+++ apps/web-main/src/app/sample/page.tsx
@@ -10,6 +10,17 @@ const SamplePage = () => {
         <button className="btn btn-lg">Large</button>
         <button className="btn btn-xl">Xlarge</button>
       </div>
+      <details className="dropdown">
+        <summary className="btn m-1">open or close</summary>
+        <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+          <li>
+            <a>Item 1</a>
+          </li>
+          <li>
+            <a>Item 2</a>
+          </li>
+        </ul>
+      </details>
     </div>
   );
 };
